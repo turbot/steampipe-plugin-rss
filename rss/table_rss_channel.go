6790dbc4ac7c3ee7a769644b67f7d2b2357ccfb5
Ved misra <47312748+misraved@users.noreply.github.com>
2022-04-27 21:06:53 +0530

    Recompile plugin with steampipe-plugin-sdk-v3.1.0 and go v1.18 (#16)
    * Recompile plugin with steampipe-plugin-sdk-v3.1.0 and go version v1.18
    
    * Update SDK

--- rss/table_rss_channel.go
+++ rss/table_rss_channel.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/mmcdole/gofeed"
 
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 func tableRSSChannel(ctx context.Context) *plugin.Table {
