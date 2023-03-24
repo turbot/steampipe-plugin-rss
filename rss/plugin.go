7c755741a7cd53b8fac099af04cee06af3a4a7e8
Jon Udell <jon@jonudell.info>
2023-03-24 08:28:13 -0700

    ignore errors

--- rss/plugin.go
+++ rss/plugin.go
@@ -10,6 +10,7 @@ import (
 func Plugin(ctx context.Context) *plugin.Plugin {
 	p := &plugin.Plugin{
 		Name:             "steampipe-plugin-rss",
+		DefaultShouldIgnoreError: isNotFoundError([]string{"403","404","500"}),
 		DefaultTransform: transform.FromGo().NullIfZero(),
 		TableMap: map[string]*plugin.Table{
 			"rss_channel": tableRSSChannel(ctx),
--- rss/utils.go
+++ rss/utils.go
@@ -2,6 +2,7 @@ package rss
 
 import (
 	"context"
+	"strings"
 
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 )
@@ -10,4 +11,16 @@ func feedLink(_ context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (in
 	quals := d.EqualsQuals
 	fl := quals["feed_link"].GetStringValue()
 	return fl, nil
+}
+
+func isNotFoundError(notFoundErrors []string) plugin.ErrorPredicate {
+	return func(err error) bool {
+
+		for _, pattern := range notFoundErrors {
+			if strings.Contains(err.Error(), pattern) {
+				return true
+			}
+		}
+		return false
+	}
 }
